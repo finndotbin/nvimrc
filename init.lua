430403e4c155962d3e25806d1f4f782f3c5cc6d6
finndotbin <vvoid127@gmail.com>
2024-06-18 21:40:09 -0700

    add more remaps

--- init.lua
+++ init.lua
@@ -4,7 +4,16 @@ vim.g.maplocalleader = " "
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("i", "kk", "<Esc>l")
 
-vim.keymap.set("n", " ex", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
+
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
+
+vim.keymap.set("n", "<leader>o", "<C-w><C-o>")
+
+vim.keymap.set("n", "<leader>/", "<cmd>noh")
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 2
@@ -17,10 +26,9 @@ vim.opt.cursorline = false
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.showmode = false
+vim.opt.showmode = true
 
 vim.opt.clipboard = "unnamed"
-
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then

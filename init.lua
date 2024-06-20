49032c6ff5c07596aae7b4209fcfa728d1ab5965
finndotbin <vvoid127@gmail.com>
2024-06-19 17:17:57 -0700

    remove redundant remaps

--- init.lua
+++ init.lua
@@ -6,13 +6,6 @@ vim.keymap.set("i", "kk", "<Esc>l")
 
 vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
 
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
-
-vim.keymap.set("n", "<leader>o", "<C-w><C-o>")
-
 vim.keymap.set("n", "<leader>/", "<Cmd>noh<CR>")
 
 vim.opt.tabstop = 4

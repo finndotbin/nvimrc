b205e493ce16281176dbb35585ff8ab2b01b9cfe
finndotbin <vvoid127@gmail.com>
2024-11-20 15:41:58 -0800

    add stylua formatting

--- lua/plugins/lspzero.lua
+++ lua/plugins/lspzero.lua
@@ -1,71 +1,66 @@
 return {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v3.x",
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
   dependencies = {
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/nvim-cmp" },
-    { "L3MON4D3/LuaSnip" },
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/cmp-nvim-lsp' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'L3MON4D3/LuaSnip' },
   },
   config = function()
-    local lsp_zero = require("lsp-zero")
+    local lspconfig = require 'lspconfig'
 
-    lsp_zero.on_attach(function(_, bufnr)
-      -- see :help lsp-zero-keybindings
-      -- to learn the available actions
-      lsp_zero.default_keymaps({
-        buffer = bufnr,
-        preserve_mappings = false,
-      })
-
-      lsp_zero.buffer_autoformat()
-    end)
-
-    local lspconfig = require("lspconfig")
-
-    lspconfig.gleam.setup({})
+    lspconfig.gleam.setup {}
 
     local configs = {
       lua_ls = {
         settings = {
           Lua = {
             diagnostics = {
-              globals = { "vim" },
+              globals = { 'vim' },
+            },
+            workspace = {
+              checkThirdParty = false,
+              telemetry = { enable = false },
+              library = {
+                vim.env.VIMRUNTIME,
+                '${3rd}/love2d/library',
+              },
             },
           },
         },
       },
     }
 
-    require("mason").setup({})
-    require("mason-lspconfig").setup({
-      ensure_installed = { "rust_analyzer", "clangd", "lua_ls" },
+    require('mason').setup {}
+    require('mason-lspconfig').setup {
+      ensure_installed = { 'rust_analyzer', 'clangd', 'lua_ls' },
       handlers = {
         function(server_name)
           lspconfig[server_name].setup(configs[server_name] or {})
         end,
       },
-    })
+    }
 
-    local cmp = require("cmp")
+    local cmp = require 'cmp'
 
-    cmp.setup({
+    cmp.setup {
       sources = {
-        { name = "nvim_lsp" },
+        { name = 'nvim_lsp' },
       },
       mapping = {
-        ["<C-y>"] = cmp.mapping.confirm({ select = false }),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = "select" }),
-        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = "select" }),
+        ['<C-y>'] = cmp.mapping.confirm { select = false },
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<C-p>'] = cmp.mapping.select_prev_item { behavior = 'select' },
+        ['<C-n>'] = cmp.mapping.select_next_item { behavior = 'select' },
       },
       snippet = {
         expand = function(args)
-          require("luasnip").lsp_expand(args.body)
+          require('luasnip').lsp_expand(args.body)
         end,
       },
-    })
-  end
+    }
+  end,
 }
